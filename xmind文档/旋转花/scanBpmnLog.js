2e6bc21719c8d2121b63ba3b633cfef020849972
custom <1316138287@qq.com>
2018-12-06 13:34:30 +0800

    升级旋转花

--- xmind文档/旋转花/scanBpmnLog.js
+++ xmind文档/旋转花/scanBpmnLog.js
@@ -13,7 +13,7 @@ $().ready(function(){
         corners: 1, 		// 花瓣圆滑度 (0-1)
         rotate: 0, 			// 花瓣旋转角度
         direction: 1, 		// 花瓣旋转方向 1: 顺时针, -1: 逆时针
-        color: '#5060f3', 	// 花瓣颜色
+        color: '#6f91f3', 	// 花瓣颜色
         speed: 1,			// 花瓣旋转速度
         trail: 80, 			// 花瓣旋转时的拖影(百分比)
         shadow: false, 		// 花瓣是否显示阴影
@@ -21,13 +21,22 @@ $().ready(function(){
         className: 'spinner', // spinner css 样式名称
         zIndex: 2e9 		// spinner的z轴 (默认是2000000000)
     });
+    function getRandomColor(){
+        return "#"+("00000"+((Math.random()*16777215+0.5)>>0).toString(16)).slice(-6);
+    }
     sp.spin(target)
+    setInterval(function(){
+        sp.opts.color=getRandomColor();
+        sp.spin(target)
+    },2000)
     $("#abc").click(function(){
         sp.spin();
     })
-    setTimeout(function(){sp.spin();alert("关闭花");$(".loading").hide()},4000)
+    // setTimeout(function(){sp.spin();alert("关闭花");$(".loading").hide()},4000)
+    // setInterval(function(){sp.spin();alert("关闭花");$(".loading").hide()},4000)
 
-
+    // setTimeout(function(){sp.spin(target);alert("打开花");$(".loading").show()},2000)
+    // setInterval(function(){sp.spin(target);alert("打开花");$(".loading").show()},2000)
 
 
 });
