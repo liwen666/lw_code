a54a79dbfcb7fb3fe87b1c8d000750b0e48eaf4b
custom <1316138287@qq.com>
2018-12-21 13:58:03 +0800

    linux的hostname设置和无密码登录设置

--- tempcode/src/test/java/com/temp/code/jedis/casual/ListTest.java
+++ tempcode/src/test/java/com/temp/code/jedis/casual/ListTest.java
@@ -2,9 +2,7 @@ package com.temp.code.jedis.casual;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ListTest {
     @Test
@@ -18,5 +16,21 @@ public class ListTest {
         T[] a = Arrays.copyOf(original, t.size());
         T[] expect = t.toArray(a);
         return expect;
+    }
+
+    @Test
+    public void treeMapTest() throws Exception {
+
+        //找到绝对小于一个数的最大数
+        TreeMap<Long,String> tree = new TreeMap<>();
+        tree.put(10L,"a");
+        tree.put(99L,"a");
+        tree.put(100L,"b");
+        tree.put(150L,"b");
+        tree.put(200L,"x");
+        Map.Entry<Long, String> longStringEntry = tree.lowerEntry( 10+1L);
+        Map.Entry<Long, String> longStringEntry2 = tree.lowerEntry( 99L);
+        System.out.println(longStringEntry.getValue()+"--"+longStringEntry.getKey());
+        System.out.println(longStringEntry2.getValue()+"---"+longStringEntry2.getKey());
     }
 }
